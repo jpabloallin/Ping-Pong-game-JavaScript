bbb17a478db2a8219e1280a20df29eb4d3951d4b
jpabloallin <jpac.647@gmail.com>
2022-04-28 16:09:26 -0500

    creación archivo main.js ---> función que me va  a general la pizarra del juego

---
+++ main.js
@@ -0,0 +1,21 @@
+(function () {
+    self.Board = function (width, height) {
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+        this.playing = false;
+    };
+
+    self.Board.prototype = {
+        get elements() {
+            var elements = this.bars.map(function (bar) {
+                return bar;
+            });
+            elements.push(this.ball);
+            return elements;
+        },
+    };
+})();
