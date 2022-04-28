abdeca5aa79636def499a5ea30a2c5298ee3cd88
jpabloallin <jpac.647@gmail.com>
2022-04-28 16:57:26 -0500

    Correción de errores

--- main.js
+++ main.js
@@ -19,120 +19,50 @@
         },
     };
 })();
-
 (function () {
     self.Ball = function (x, y, radius, board) {
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.speed_y = 0;
-      this.speed_x = 3;
-      this.board = board;
-      this.direction = 1;
-      this.bounce_angle = 0;
-      this.max_bounce_angle = Math.PI / 12;
-      this.speed = 7;
-  
-      board.ball = this;
-      this.kind = "circle";
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 7;
+
+        board.ball = this;
+        this.kind = "circle";
     };
-  
+
     self.Ball.prototype = {
-      move: function () {
-        this.x += this.speed_x * this.direction;
-        this.y += this.speed_y;
-      },
-      get width() {
-        return this.radius * 2;
-      },
-      get height() {
-        return this.radius * 2;
-      },
-  
-      collision: function (bar) {
-        var relative_intersect_y = bar.y + bar.height / 2 - this.y;
-  
-        var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
-  
-        this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
-  
-        this.speed_y = this.speed * -Math.sin(this.bounce_angle);
-        this.speed_x = this.speed * Math.cos(this.bounce_angle);
-  
-        if (this.x > this.board.width / 2) this.direction = -1;
-        else this.direction = 1;
-      },
+        move: function () {
+            this.x += this.speed_x * this.direction;
+            this.y += this.speed_y;
+        },
+        get width() {
+            return this.radius * 2;
+        },
+        get height() {
+            return this.radius * 2;
+        },
+
+        collision: function (bar) {
+            var relative_intersect_y = bar.y + bar.height / 2 - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > this.board.width / 2) this.direction = -1;
+            else this.direction = 1;
+        },
     };
 })();
-(function () {
-    self.BoardView = function (canvas, board) {
-      this.canvas = canvas;
-      this.canvas.width = board.width;
-      this.canvas.height = board.height;
-      this.board = board;
-      this.cxt = canvas.getContext("2d");
-    };
-
-    self.BoardView.prototype = {
-        clean: function () {
-          this.cxt.clearRect(0, 0, this.board.width, this.board.height);
-        },
-        draw: function () {
-          for (var i = this.board.elements.length - 1; i >= 0; i--) {
-            var el = this.board.elements[i];
-    
-            draw(this.cxt, el);
-          }
-        },
-        check_collisions: function () {
-            for (var i = this.board.bars.length - 1; i >= 0; i--) {
-              var bar = this.board.bars[i];
-              if (hit(bar, this.board.ball)) {
-                this.board.ball.collision(bar);
-              }
-            }
-          },
-          play: function () {
-            if (this.board.playing) {
-              this.clean();
-              this.draw();
-              this.check_collisions();
-              this.board.ball.move();
-            }
-          },
-        };
-        function hit(a, b) {
-          var hit = false;
-      
-          if (b.x + b.width >= a.x && b.x < a.x + a.width) {
-            if (b.y + b.height >= a.y && b.y < a.y + a.height) hit = true;
-          }
-      
-          if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
-            if (b.y <= a.y && b.y + b.height >= a.y + a.height) hit = true;
-          }
-      
-          if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
-            if (a.y <= b.y && a.y + a.height >= b.y + b.height) hit = true;
-          }
-          return hit;
-        }
-
-    function draw(cxt, element) {
-        switch (element.kind) {
-            case "rectangle":
-                cxt.fillRect(element.x, element.y, element.width, element.height);
-                break;
-        
-            case "circle":
-                cxt.beginPath();
-                cxt.arc(element.x, element.y, element.radius, 0, 7);
-                cxt.fill();
-                cxt.closePath();
-                break;
-            }
-          }
-
 (function () {
     self.Bar = function (x, y, width, height, board) {
         this.x = x;
@@ -144,6 +74,7 @@
         this.kind = "rectangle";
         this.speed = 20;
     };
+
     self.Bar.prototype = {
         down: function () {
             this.y += this.speed;
@@ -154,10 +85,77 @@
         toString: function () {
             return "x: " + this.x + "y: " + this.y;
         },
-      };
+    };
 })();
+(function () {
+    self.BoardView = function (canvas, board) {
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.cxt = canvas.getContext("2d");
+    };
 
+    self.BoardView.prototype = {
+        clean: function () {
+            this.cxt.clearRect(0, 0, this.board.width, this.board.height);
+        },
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
 
+                draw(this.cxt, el);
+            }
+        },
+        check_collisions: function () {
+            for (var i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
+        play: function () {
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.check_collisions();
+                this.board.ball.move();
+            }
+        },
+    };
+    function hit(a, b) {
+        var hit = false;
+
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            if (b.y + b.height >= a.y && b.y < a.y + a.height) hit = true;
+        }
+
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height) hit = true;
+        }
+
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height) hit = true;
+        }
+        return hit;
+    }
+
+    function draw(cxt, element) {
+        switch (element.kind) {
+            case "rectangle":
+                cxt.fillRect(element.x, element.y, element.width, element.height);
+                break;
+
+            case "circle":
+                cxt.beginPath();
+                cxt.arc(element.x, element.y, element.radius, 0, 7);
+                cxt.fill();
+                cxt.closePath();
+                break;
+        }
+    }
+})();
 var board = new Board(800, 400);
 var bar = new Bar(20, 150, 40, 100, board);
 var bar_2 = new Bar(740, 150, 40, 100, board);
@@ -167,25 +165,28 @@ var ball = new Ball(400, 200, 10, board);
 
 document.addEventListener("keydown", function (ev) {
     if (ev.keyCode == 103) {
-      ev.preventDefault();
-      bar.up();
+        ev.preventDefault();
+        bar.up();
     } else if (ev.keyCode == 97) {
-      ev.preventDefault();
-      bar.down();
+        ev.preventDefault();
+        bar.down();
     } else if (ev.keyCode == 105) {
-      ev.preventDefault();
-      bar_2.up();
+        ev.preventDefault();
+        bar_2.up();
     } else if (ev.keyCode == 99) {
-      ev.preventDefault();
-      bar_2.down();
+        ev.preventDefault();
+        bar_2.down();
     } else if (ev.keyCode === 96) {
-      ev.preventDefault();
-      board.playing = !board.playing;
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 });
 
 board_view.draw();
 window.requestAnimationFrame(controller);
+setTimeout(function () {
+    ball.direction = -2;
+}, 4000);
 
 function controller() {
     board_view.play();
